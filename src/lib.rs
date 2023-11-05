9b6b31a7527dfe2900fa365ff93e14220778c264
night-crawler <lilo.panic@gmail.com>
2023-11-05 12:37:35 +0000

    added non-cloning find method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::str::FromStr;
-
-use jsonpath_rust::{JsonPathFinder, JsonPathInst, JsonPathValue, JsonPtr};
+use jsonpath_rust::JsonPathValue;
+use jsonpath_rust::parser::model::JsonPath;
+use jsonpath_rust::path::json_path_instance;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pythonize::{depythonize, pythonize};
@@ -31,102 +31,52 @@ impl Finder {
         Ok(Self { value: parse_py_object(obj)? })
     }
 
-    fn find_with_paths(self_: PyRef<'_, Self>, query: String) -> PyResult<Vec<JsonPathResult>> {
-        let value = &self_.value;
-        let query = parse_query(&query)?;
-
-        let finder = JsonPathFinder::new(value.clone().into(), query.into());
-        let slice = finder.find_slice();
-        Python::with_gil(|py| {
-            map_json_path_values(py, slice)
-        })
-    }
 
     fn find(self_: PyRef<'_, Self>, query: String) -> PyResult<Vec<JsonPathResult>> {
         let value = &self_.value;
         let query = parse_query(&query)?;
-        let slice = query.find_slice(value);
+
+        let slice = json_path_instance(&query, value)
+            .find(JsonPathValue::from_root(value))
+            .into_iter()
+            .collect::<Vec<_>>();
 
         Python::with_gil(|py| {
-            map_json_ptrs(py, slice)
+            slice.into_iter().map(|v| map_json_path_value(py, v)).collect()
         })
     }
 }
 
 
-#[pyfunction]
-fn jsonpath(query: String, obj: PyObject) -> PyResult<Vec<JsonPathResult>> {
-    let query = parse_query(&query)?;
-    let value = parse_py_object(obj)?;
-    let finder = JsonPathFinder::new(value.into(), query.into());
-    let slice = finder.find_slice();
-    Python::with_gil(|py| {
-        map_json_path_values(py, slice)
+fn map_json_path_value(py: Python, jpv: JsonPathValue<Value>) -> PyResult<JsonPathResult> {
+    Ok(match jpv {
+        JsonPathValue::Slice(data, path) => {
+            JsonPathResult {
+                data: Some(pythonize(py, data)?),
+                path: Some(path.to_string()),
+                is_new_value: false,
+            }
+        }
+        JsonPathValue::NewValue(data) => {
+            JsonPathResult {
+                data: Some(pythonize(py, &data)?),
+                path: None,
+                is_new_value: true,
+            }
+        }
+        JsonPathValue::NoValue => {
+            JsonPathResult {
+                data: None,
+                path: None,
+                is_new_value: false,
+            }
+        }
     })
 }
 
 
-fn map_json_ptrs(py: Python, slice: Vec<JsonPtr<Value>>) -> PyResult<Vec<JsonPathResult>> {
-    let mut result = vec![];
-
-    for node in slice {
-        match node {
-            JsonPtr::Slice(data) => {
-                result.push(JsonPathResult {
-                    data: Some(pythonize(py, data)?),
-                    path: None,
-                    is_new_value: false,
-                });
-            }
-            JsonPtr::NewValue(data) => {
-                result.push(JsonPathResult {
-                    data: Some(pythonize(py, &data)?),
-                    path: None,
-                    is_new_value: true,
-                });
-            }
-        }
-    }
-
-    Ok(result)
-}
-
-
-fn map_json_path_values(py: Python, slice: Vec<JsonPathValue<Value>>) -> PyResult<Vec<JsonPathResult>> {
-    let mut result = vec![];
-
-    for node in slice {
-        match node {
-            JsonPathValue::Slice(data, path) => {
-                result.push(JsonPathResult {
-                    data: Some(pythonize(py, data)?),
-                    path: Some(path.to_string()),
-                    is_new_value: false,
-                });
-            }
-            JsonPathValue::NewValue(data) => {
-                result.push(JsonPathResult {
-                    data: Some(pythonize(py, &data)?),
-                    path: None,
-                    is_new_value: true,
-                });
-            }
-            JsonPathValue::NoValue => {
-                result.push(JsonPathResult {
-                    data: None,
-                    path: None,
-                    is_new_value: false,
-                })
-            }
-        }
-    }
-
-    Ok(result)
-}
-
-
-fn parse_query(query: &str) -> PyResult<JsonPathInst> {
-    match JsonPathInst::from_str(query) {
+fn parse_query(query: &str) -> PyResult<JsonPath> {
+    match JsonPath::try_from(query) {
         Ok(inst) => Ok(inst),
         Err(err) => {
             Err(PyValueError::new_err(err))
@@ -144,6 +94,5 @@ fn parse_py_object(obj: PyObject) -> PyResult<Value> {
 #[pymodule]
 fn jsonpath_rust_bindings(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Finder>()?;
-    m.add_function(wrap_pyfunction!(jsonpath, m)?)?;
     Ok(())
 }
