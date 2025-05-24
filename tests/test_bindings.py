70d1d806a5264b1c0d6ab47f2665f2afe964cf49
night-crawler <lilo.panic@gmail.com>
2025-05-24 15:23:33 +0100

    migrate to version 1.0.2

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -12,32 +12,35 @@ def sample_data() -> dict:
                     'category': 'reference',
                     'author': 'Nigel Rees',
                     'title': 'Sayings of the Century',
-                    'price': 8.95,
+                    'price': 8.95
                 },
                 {
                     'category': 'fiction',
                     'author': 'Evelyn Waugh',
                     'title': 'Sword of Honour',
-                    'price': 12.99,
+                    'price': 12.99
                 },
                 {
                     'category': 'fiction',
                     'author': 'Herman Melville',
                     'title': 'Moby Dick',
                     'isbn': '0-553-21311-3',
-                    'price': 8.99,
+                    'price': 8.99
                 },
                 {
                     'category': 'fiction',
                     'author': 'J. R. R. Tolkien',
                     'title': 'The Lord of the Rings',
                     'isbn': '0-395-19395-8',
-                    'price': 22.99,
-                },
+                    'price': 22.99
+                }
             ],
-            'bicycle': {'color': 'red', 'price': 19.95},
+            'bicycle': {
+                'color': 'red',
+                'price': 19.95
+            }
         },
-        'expensive': 10,
+        'expensive': 10
     }
 
 
@@ -58,18 +61,39 @@ def test_exceptions(sample_data):
 def test_repr(sample_data):
     finder = Finder(sample_data)
     result = str(finder.find('$.store.bicycle.color')[0])
-    assert result == """JsonPathResult(data='red', path="$.['store'].['bicycle'].['color']", is_new_value=False)"""
+    assert result == """JsonPathResult(data='red', path="$['store']['bicycle']['color']")"""
 
 
-def test_non_empty(sample_data):
-    finder = Finder(sample_data)
+def test_smoke_queries(sample_data):
+    queries = [
+        '$.store.book[*].author',
+        '$..book[?(@.isbn)]',
+        '$.store.*',
+        '$..author',
+        '$.store..price',
+        '$..book[2]',
+        '$..book[-2]',
+        '$..book[0,1]',
+        '$..book[:2]',
+        '$..book[1:2]',
+        '$..book[-2:]',
+        '$..book[2:]',
+        '$.store.book[?(@.price<10)]',
+        '$..book[?(@.price<=$.expensive)]',
+        # "$..book[?@.author ~= '(?i)REES']",
+        '$..*',
+    ]
 
-    result = finder.find("..*[?(@.category=='fiction')]")
-    for entry in result:
-        assert entry.path is not None and entry.data is not None
+    f = Finder(sample_data)
 
-    # after jsonpath-rust version bump this method does not seem to be needed,
-    # though let it be for some time just in case
-    result = finder.find_non_empty("..*[?(@.category=='fiction')]")
-    for entry in result:
-        assert entry.path is not None and entry.data is not None
+    for query in queries:
+        print(query)
+        print(f.find(query), '\n')
+        print('----------')
+
+
+def test_overflow():
+    big_number = 18446744005107584948
+    f = Finder({"test": big_number})
+    res = f.find('$.test')[0].data
+    assert res == big_number
