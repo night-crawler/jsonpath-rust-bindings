a0eafc44ea9322ce6aebad537ea0962e326f8afc
wenming21 <wen-ming21@qq.com>
2025-10-07 23:02:17 +0800

    migrate to version 1.1.0 (#7)
    * migrate to version 1.1.0
    
    1. Optimize the code and use the mimalloc memory allocator to improve the running speed of the Python binding.
    2. The find_data and find_absolute_path methods have been added to Finder, which return the matching value and absolute path respectively.
    3. test_bindings.py all tests passed, running on Windows 10 with python version 3.13.7.
    4. Comments are added in lib.rs.
    
    * Add files via upload
    
    * Remove the cache and add support for Python 3.14
    
    * add support for python 3.14
    
    * Remove the cache
    
    * Update test.yml
    
    * Add files via upload
    
    * fix build errors
    
    * modify lib.rs
    
    * Update lib.rs
    
    * Update Cargo.toml
    
    * Add support for Python 3.14
    
    * Add files via upload
    
    * Add support for Python 3.14
    
    * Delete .github/workflows/workflows directory
    
    * Add support for Python 3.14
    
    * Add support for Python 3.14
    
    * Add support for Python 3.14
    
    * Delete .github/workflows/CI.yml
    
    * Delete .github/workflows/test.yml
    
    * Delete .github/workflows directory
    
    * Add support for Python 3.14
    
    * Delete github/workflows directory
    
    * Create .github
    
    * Delete .github
    
    * Add support for Python 3.14
    
    * Add support for Python 3.14
    
    * Add support for Python 3.14
    
    * Add support for Python 3.14 and pypy3.11
    
    * fix some problems
    
    * Optimize windows workflow

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,45 @@ use pyo3::prelude::*;
 use pythonize::{depythonize, pythonize};
 use serde_json::Value;
 
+#[cfg(any(
+    not(target_os = "linux"),
+    all(target_os = "linux", target_env = "musl")
+))]
+use mimalloc::MiMalloc;
+#[cfg(any(
+    not(target_os = "linux"),
+    all(target_os = "linux", target_env = "musl")
+))]
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+#[cfg(all(target_os = "linux", not(target_env = "musl")))]
+use std::alloc::System;
+#[cfg(all(target_os = "linux", not(target_env = "musl")))]
+#[global_allocator]
+static GLOBAL: System = System;
+
+// JSONPath query result containing found data and path
 #[pyclass(frozen)]
 struct JsonPathResult {
+    // Found data value
     #[pyo3(get)]
-    data: Option<PyObject>,
+    data: Option<Py<PyAny>>,
 
+    // Path of the data in JSON
     #[pyo3(get)]
     path: Option<String>,
 }
 
 #[pymethods]
 impl JsonPathResult {
+    // Returns string representation of JsonPathResult
     fn __repr__(slf: PyRef<'_, Self>) -> PyResult<String> {
         repr_json_path_result(slf)
     }
 }
 
+// JSONPath finder for executing queries on JSON data
 #[pyclass(frozen)]
 struct Finder {
     value: Value,
@@ -30,32 +53,50 @@ struct Finder {
 #[pymethods]
 impl Finder {
     #[new]
-    fn py_new(obj: PyObject) -> PyResult<Self> {
+    fn py_new(obj: Py<PyAny>) -> PyResult<Self> {
         Ok(Self {
             value: parse_py_object(obj)?,
         })
     }
 
+    // Execute JSONPath query, return list of results containing data and paths
     fn find(self_: PyRef<'_, Self>, query: String) -> PyResult<Vec<JsonPathResult>> {
-        find_internal(&self_.value, &query, |_| true)
+        find_internal_path_value(&self_.value, &query, |_| true)
+    }
+
+    // Execute JSONPath query, return only found data values
+    fn find_data(self_: PyRef<'_, Self>, query: String) -> PyResult<Vec<Py<PyAny>>> {
+        find_internal_data(&self_.value, &query, |_| true)
+    }
+
+    // Execute JSONPath query, return only found absolute paths
+    fn find_absolute_path(self_: PyRef<'_, Self>, query: String) -> PyResult<Vec<String>> {
+        find_internal_path(&self_.value, &query, |_| true)
     }
 }
 
-fn find_internal(
+// Execute JSONPath query and return processed results
+fn execute_query<'a>(
+    value: &'a Value,
+    query: &str,
+    predicate: impl Fn(&QueryRef<Value>) -> bool,
+) -> PyResult<Vec<QueryRef<'a, Value>>> {
+    let parsed_query = parse_query(query)?;
+    let processed = js_path_process(&parsed_query, value)
+        .map_err(|err| PyValueError::new_err(err.to_string()))?;
+
+    Ok(processed.into_iter().filter(predicate).collect())
+}
+
+// Execute query and return JsonPathResult list
+fn find_internal_path_value(
     value: &Value,
     query: &str,
     predicate: impl Fn(&QueryRef<Value>) -> bool,
 ) -> PyResult<Vec<JsonPathResult>> {
-    let query = parse_query(query)?;
-    let processed = match js_path_process(&query, value) {
-        Ok(p) => p,
-        Err(err) => {
-            return Err(PyValueError::new_err(err.to_string()));
-        }
-    };
-    let filtered = processed.into_iter().filter(predicate);
+    let filtered = execute_query(value, query, predicate)?;
 
-    Python::with_gil(|py| {
+    Python::attach(|py| {
         filtered
             .into_iter()
             .map(|v| map_json_path_value(py, v))
@@ -63,44 +104,76 @@ fn find_internal(
     })
 }
 
+// Execute query and return data value list
+fn find_internal_data(
+    value: &Value,
+    query: &str,
+    predicate: impl Fn(&QueryRef<Value>) -> bool,
+) -> PyResult<Vec<Py<PyAny>>> {
+    let filtered = execute_query(value, query, predicate)?;
+
+    Python::attach(|py| {
+        filtered
+            .into_iter()
+            .map(|v| map_json_value(py, v))
+            .collect()
+    })
+}
+
+// Execute query and return path string list
+fn find_internal_path(
+    value: &Value,
+    query: &str,
+    predicate: impl Fn(&QueryRef<Value>) -> bool,
+) -> PyResult<Vec<String>> {
+    let filtered = execute_query(value, query, predicate)?;
+    filtered.into_iter().map(|v| map_json_path(v)).collect()
+}
+
+// Map QueryRef<Value> to JsonPathResult
 fn map_json_path_value(py: Python, jpv: QueryRef<Value>) -> PyResult<JsonPathResult> {
     let path = jpv.clone().path();
     let val = jpv.val();
 
-    let res = JsonPathResult {
+    Ok(JsonPathResult {
         data: Some(pythonize(py, val)?.into_pyobject(py)?.unbind()),
         path: Some(path),
-    };
-
-    Ok(res)
-}
-
-fn parse_query(query: &str) -> PyResult<JpQuery> {
-    match parse_json_path(query) {
-        Ok(inst) => Ok(inst),
-        Err(err) => Err(PyValueError::new_err(format!("{err:?}"))),
-    }
-}
-
-fn parse_py_object(obj: PyObject) -> PyResult<Value> {
-    Python::with_gil(|py| {
-        let any = obj.downcast_bound::<PyAny>(py)?.clone().into_any();
-        let value = depythonize(&any)?;
-        Ok(value)
     })
 }
 
+// Extract path string from QueryRef<Value>
+fn map_json_path(jpv: QueryRef<Value>) -> PyResult<String> {
+    Ok(jpv.path())
+}
+
+// Convert value in QueryRef<Value> to Python object
+fn map_json_value(py: Python, jpv: QueryRef<Value>) -> PyResult<Py<PyAny>> {
+    let val = jpv.val();
+    Ok(pythonize(py, val)?.into_pyobject(py)?.unbind())
+}
+
+// Parse JSONPath query string directly without caching
+fn parse_query(query: &str) -> PyResult<JpQuery> {
+    parse_json_path(query).map_err(|err| PyValueError::new_err(format!("{err:?}")))
+}
+
+// Convert Python object to serde_json::Value
+fn parse_py_object(obj: Py<PyAny>) -> PyResult<Value> {
+    Python::attach(|py| {
+        let any = obj.bind(py);
+        depythonize(any).map_err(|e| PyValueError::new_err(e.to_string()))
+    })
+}
+
+// Generate string representation for JsonPathResult
 fn repr_json_path_result(slf: PyRef<'_, JsonPathResult>) -> PyResult<String> {
     let data_repr = slf
         .data
         .as_ref()
-        .map(|data| Python::with_gil(|py| format!("{:?}", data.bind(py))))
+        .map(|data| Python::attach(|py| format!("{:?}", data.bind(py))))
         .unwrap_or_default();
 
-    let path_repr = match &slf.path {
-        Some(path) => path,
-        None => "None",
-    };
+    let path_repr = slf.path.as_deref().unwrap_or("None");
     Ok(format!(
         "JsonPathResult(data={data_repr}, path={path_repr:?})",
     ))
--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -6,47 +6,44 @@ from jsonpath_rust_bindings import JsonPathResult
 @pytest.fixture
 def sample_data() -> dict:
     return {
-        'store': {
-            'book': [
+        "store": {
+            "book": [
                 {
-                    'category': 'reference',
-                    'author': 'Nigel Rees',
-                    'title': 'Sayings of the Century',
-                    'price': 8.95
+                    "category": "reference",
+                    "author": "Nigel Rees",
+                    "title": "Sayings of the Century",
+                    "price": 8.95,
                 },
                 {
-                    'category': 'fiction',
-                    'author': 'Evelyn Waugh',
-                    'title': 'Sword of Honour',
-                    'price': 12.99
+                    "category": "fiction",
+                    "author": "Evelyn Waugh",
+                    "title": "Sword of Honour",
+                    "price": 12.99,
                 },
                 {
-                    'category': 'fiction',
-                    'author': 'Herman Melville',
-                    'title': 'Moby Dick',
-                    'isbn': '0-553-21311-3',
-                    'price': 8.99
+                    "category": "fiction",
+                    "author": "Herman Melville",
+                    "title": "Moby Dick",
+                    "isbn": "0-553-21311-3",
+                    "price": 8.99,
                 },
                 {
-                    'category': 'fiction',
-                    'author': 'J. R. R. Tolkien',
-                    'title': 'The Lord of the Rings',
-                    'isbn': '0-395-19395-8',
-                    'price': 22.99
-                }
+                    "category": "fiction",
+                    "author": "J. R. R. Tolkien",
+                    "title": "The Lord of the Rings",
+                    "isbn": "0-395-19395-8",
+                    "price": 22.99,
+                },
             ],
-            'bicycle': {
-                'color': 'red',
-                'price': 19.95
-            }
+            "bicycle": {"color": "red", "price": 19.95},
         },
-        'expensive': 10
+        "expensive": 10,
     }
 
 
 def test_sanity(sample_data):
     finder = Finder(sample_data)
-    res = finder.find('$..book[?(@.price<=$.expensive)]')
+    res = finder.find("$..book[?(@.price<=$.expensive)]")
     assert len(res) == 2
     assert isinstance(res[0], JsonPathResult)
 
@@ -55,41 +52,145 @@ def test_exceptions(sample_data):
     finder = Finder(sample_data)
 
     with pytest.raises(ValueError):
-        finder.find('fail')
+        finder.find("fail")
 
 
 def test_repr(sample_data):
     finder = Finder(sample_data)
-    result = str(finder.find('$.store.bicycle.color')[0])
-    assert result == """JsonPathResult(data='red', path="$['store']['bicycle']['color']")"""
+    result = str(finder.find("$.store.bicycle.color")[0])
+    assert (
+        result
+        == """JsonPathResult(data='red', path="$['store']['bicycle']['color']")"""
+    )
 
 
-def test_smoke_queries(sample_data):
+def test_all_queries(sample_data):
     queries = [
-        '$.store.book[*].author',
-        '$..book[?(@.isbn)]',
-        '$.store.*',
-        '$..author',
-        '$.store..price',
-        '$..book[2]',
-        '$..book[-2]',
-        '$..book[0,1]',
-        '$..book[:2]',
-        '$..book[1:2]',
-        '$..book[-2:]',
-        '$..book[2:]',
-        '$.store.book[?(@.price<10)]',
-        '$..book[?(@.price<=$.expensive)]',
+        "$.store.book[*].author",
+        "$..book[?(@.isbn)]",
+        "$.store.*",
+        "$..author",
+        "$.store..price",
+        "$..book[2]",
+        "$..book[-2]",
+        "$..book[0,1]",
+        "$..book[:2]",
+        "$..book[1:2]",
+        "$..book[-2:]",
+        "$..book[2:]",
+        "$.store.book[?(@.price<10)]",
+        "$..book[?(@.price<=$.expensive)]",
         # "$..book[?@.author ~= '(?i)REES']",
-        '$..*',
+        "$..*",
     ]
-
+    queries_results = [
+        """[JsonPathResult(data='Nigel Rees', path="$['store']['book'][0]['author']"), 
+JsonPathResult(data='Evelyn Waugh', path="$['store']['book'][1]['author']"), 
+JsonPathResult(data='Herman Melville', path="$['store']['book'][2]['author']"), 
+JsonPathResult(data='J. R. R. Tolkien', path="$['store']['book'][3]['author']")]""",
+        """[JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]"), 
+JsonPathResult(data={'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}, 
+path="$['store']['book'][3]")]""",
+        """[JsonPathResult(data={'color': 'red', 'price': 19.95}, path="$['store']['bicycle']"), 
+JsonPathResult(data=[{'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+{'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, 
+{'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+{'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}], 
+path="$['store']['book']")]""",
+        """[JsonPathResult(data='Nigel Rees', path="$['store']['book'][0]['author']"), 
+JsonPathResult(data='Evelyn Waugh', path="$['store']['book'][1]['author']"), 
+JsonPathResult(data='Herman Melville', path="$['store']['book'][2]['author']"), 
+JsonPathResult(data='J. R. R. Tolkien', path="$['store']['book'][3]['author']")]""",
+        """[JsonPathResult(data=19.95, path="$['store']['bicycle']['price']"), 
+JsonPathResult(data=8.95, path="$['store']['book'][0]['price']"), 
+JsonPathResult(data=12.99, path="$['store']['book'][1]['price']"), 
+JsonPathResult(data=8.99, path="$['store']['book'][2]['price']"), 
+JsonPathResult(data=22.99, path="$['store']['book'][3]['price']")]""",
+        """[JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]")]""",
+        """[JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]")]""",
+        """[JsonPathResult(data={'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+path="$['store']['book'][0]"), 
+JsonPathResult(data={'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, path="$['store']['book'][1]")]""",
+        """[JsonPathResult(data={'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+path="$['store']['book'][0]"), 
+JsonPathResult(data={'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, path="$['store']['book'][1]")]""",
+        """[JsonPathResult(data={'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, 
+path="$['store']['book'][1]")]""",
+        """[JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]"), 
+JsonPathResult(data={'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}, 
+path="$['store']['book'][3]")]""",
+        """[JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]"), 
+JsonPathResult(data={'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}, 
+path="$['store']['book'][3]")]""",
+        """[JsonPathResult(data={'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+path="$['store']['book'][0]"), 
+JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]")]""",
+        """[JsonPathResult(data={'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+path="$['store']['book'][0]"), 
+JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]")]""",
+        """[JsonPathResult(data=10, path="$['expensive']"), 
+JsonPathResult(data={'bicycle': {'color': 'red', 'price': 19.95}, 
+'book': [{'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+{'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, 
+{'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+{'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}]}, 
+path="$['store']"), 
+JsonPathResult(data={'color': 'red', 'price': 19.95}, path="$['store']['bicycle']"), 
+JsonPathResult(data=[{'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+{'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, 
+{'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+{'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}], 
+path="$['store']['book']"), 
+JsonPathResult(data='red', path="$['store']['bicycle']['color']"), 
+JsonPathResult(data=19.95, path="$['store']['bicycle']['price']"), 
+JsonPathResult(data={'author': 'Nigel Rees', 'category': 'reference', 'price': 8.95, 'title': 'Sayings of the Century'}, 
+path="$['store']['book'][0]"), 
+JsonPathResult(data={'author': 'Evelyn Waugh', 'category': 'fiction', 'price': 12.99, 'title': 'Sword of Honour'}, path="$['store']['book'][1]"), 
+JsonPathResult(data={'author': 'Herman Melville', 'category': 'fiction', 'isbn': '0-553-21311-3', 'price': 8.99, 'title': 'Moby Dick'}, 
+path="$['store']['book'][2]"), 
+JsonPathResult(data={'author': 'J. R. R. Tolkien', 'category': 'fiction', 'isbn': '0-395-19395-8', 'price': 22.99, 'title': 'The Lord of the Rings'}, 
+path="$['store']['book'][3]"), 
+JsonPathResult(data='Nigel Rees', path="$['store']['book'][0]['author']"), 
+JsonPathResult(data='reference', path="$['store']['book'][0]['category']"), 
+JsonPathResult(data=8.95, path="$['store']['book'][0]['price']"), 
+JsonPathResult(data='Sayings of the Century', path="$['store']['book'][0]['title']"), 
+JsonPathResult(data='Evelyn Waugh', path="$['store']['book'][1]['author']"), 
+JsonPathResult(data='fiction', path="$['store']['book'][1]['category']"), 
+JsonPathResult(data=12.99, path="$['store']['book'][1]['price']"), 
+JsonPathResult(data='Sword of Honour', path="$['store']['book'][1]['title']"), 
+JsonPathResult(data='Herman Melville', path="$['store']['book'][2]['author']"), 
+JsonPathResult(data='fiction', path="$['store']['book'][2]['category']"), 
+JsonPathResult(data='0-553-21311-3', path="$['store']['book'][2]['isbn']"), 
+JsonPathResult(data=8.99, path="$['store']['book'][2]['price']"), 
+JsonPathResult(data='Moby Dick', path="$['store']['book'][2]['title']"), 
+JsonPathResult(data='J. R. R. Tolkien', path="$['store']['book'][3]['author']"), 
+JsonPathResult(data='fiction', path="$['store']['book'][3]['category']"), 
+JsonPathResult(data='0-395-19395-8', path="$['store']['book'][3]['isbn']"), 
+JsonPathResult(data=22.99, path="$['store']['book'][3]['price']"), 
+JsonPathResult(data='The Lord of the Rings', path="$['store']['book'][3]['title']")]""",
+    ]
     f = Finder(sample_data)
-
-    for query in queries:
-        print(query)
-        print(f.find(query), '\n')
-        print('----------')
+    res = []
+    res_data = []
+    res_absolute_path = []
+    for query, queries_result in zip(queries, queries_results):
+        temp_res = f.find(query)
+        res.extend(temp_res)
+        res_data.extend(f.find_data(query))
+        res_absolute_path.extend(f.find_absolute_path(query))
+        assert str(temp_res) == queries_result.replace("\n", "")
+        assert [r.data for r in res] == res_data
+        assert [r.path for r in res] == res_absolute_path
+        # print(query)
+        # print(f.find(query), '\n')
+        # print('----------')
 
 
 def test_overflow():
